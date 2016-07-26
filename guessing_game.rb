2647c4f868efdbec7c14c0ae67fb6b64ff19ac5c
Alan Kebert <adkebert@yahoo.com>
2016-07-26 13:28:52 -0400

    now generating random numbers

--- guessing_game.rb
+++ guessing_game.rb
@@ -3,3 +3,8 @@
 # Need to evaluate the user's guess against our random numbers
 # Need to display an appropriate message after evaluating
 # Need to either reprompt, announce victory, or announce defeat
+
+# Let's get a RNG
+rng = Random.new
+# Now we generate a number between 1-100 and put it in our_number
+our_number = rng.rand(100)
