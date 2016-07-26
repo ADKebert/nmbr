1bdf588d1cbe457485a6a5014bc8ee5e80eafa23
Alan Kebert <adkebert@yahoo.com>
2016-07-26 13:20:36 -0400

    established objectives

---
+++ guessing_game.rb
@@ -0,0 +1,5 @@
+# Need to find a way to generate random numbers
+# Need to create a reusably prompt for a guess
+# Need to evaluate the user's guess against our random numbers
+# Need to display an appropriate message after evaluating
+# Need to either reprompt, announce victory, or announce defeat
