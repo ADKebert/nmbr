4613ccdd19f27a29bfcd550e14d7560c512a3a2c
Alan Kebert <adkebert@yahoo.com>
2016-07-26 13:52:01 -0400

    explorer mode complete

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,5 +1,5 @@
 # Need to find a way to generate random numbers
-# Need to create a reusably prompt for a guess
+# Need to create a reusable prompt for a guess
 # Need to evaluate the user's guess against our random numbers
 # Need to display an appropriate message after evaluating
 # Need to either reprompt, announce victory, or announce defeat
@@ -18,3 +18,27 @@ end
 rng = Random.new
 # Now we generate a number between 1-100 and put it in our_number
 our_number = rng.rand(100)
+
+# Let's announce the rules and establish a guess counter
+puts "Try to guess my number.  It is between 1 and 100"
+count = 0
+
+# Time to begin guessing
+loop do
+  result = evaluate_guess(prompt_for_guess, our_number)
+  # I'll check for victory first because it can stop the loop earliest
+  if result == 0
+    puts "Victory!"
+    break
+  elsif result == -1
+    puts "Higher!"
+  else
+    puts "Lower!"
+  end
+  count += 1
+  # Once count == 5 the user has lost
+  if count == 5
+    puts "You Lose :("
+    break
+  end
+end
